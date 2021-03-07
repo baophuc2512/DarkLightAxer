8a00aa1f62b611e3bd9e094e1d917987059d0d04
DESKTOP-27ODUHD\nnbao <hachan.bt@gmail.com>
2021-03-07 12:30:39 +0100

    Update

--- Archer/game.js
+++ Archer/game.js
@@ -15,7 +15,7 @@ const config = {
         physics: {
             default: 'arcade',
             arcade: {
-                gravity: {y: 300},
+                gravity: {y: 500},
                 enableBody: true,
                 debug: false,
             },
