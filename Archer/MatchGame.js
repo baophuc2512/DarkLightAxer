7bcdc8469025b811e0b47aa636c1c2e4a63a4fed
DESKTOP-AD6TUM9\phuc <baophuc2512@gmail.com>
2021-03-12 16:57:51 +0700

    Animation and Shooting Update!

--- Archer/MatchGame.js
+++ Archer/MatchGame.js
@@ -10,6 +10,14 @@ class MatchGame extends Phaser.Scene {
         this.load.image('arrow','PictureAnim/Arrow.png');
         this.load.image('axe','PictureAnim/Axe.png');
         this.load.image('background','PictureAnim/Background.png');
+        this.load.spritesheet('character','PictureAnim/SkeletonWalk.png', {
+            frameWidth: 22,
+            frameHeight: 36,
+        });
+        this.load.spritesheet('characterAttack','PictureAnim/SkeletonAttack.png', {
+            frameWidth: 43,
+            frameHeight: 36,
+        });
     }
     create() {
         //Background
@@ -22,7 +30,8 @@ class MatchGame extends Phaser.Scene {
 
 
         //Player 1
-        gameState.character = this.physics.add.sprite(100,100,'player');
+        gameState.character = this.physics.add.sprite(100,100,'character');
+        gameState.character.setScale(3);
         this.physics.add.collider(gameState.character,gameState.platform);
 
         //Player 2
@@ -30,7 +39,7 @@ class MatchGame extends Phaser.Scene {
         this.physics.add.collider(gameState.character2,gameState.platform);
 
         
-        //Arrow clone create.
+        //Arrow clone create. (Player 1);
         var Arrow = new Phaser.Class({
 
             Extends: Phaser.Physics.Arcade.Image,
@@ -56,12 +65,47 @@ class MatchGame extends Phaser.Scene {
                     let x = this.speed * delta;
                     this.x += x - 3;
                     this.y += -4*x;
-                } else if (this.type === 'L') {
-                    // this.x -= this.speed * delta;
-                    // this.y -= this.speed * delta;
-                }
+                } 
 
-                if (this.x > gameState.playerOldX + 2000 || this.x < gameState.playerOldX - 2000 || gameState.takeShootCharac2 == 1) {
+                if (this.x > gameState.playerOldX + 2000 || this.x < gameState.playerOldX - 2000 || gameState.takeShootCharac == 1 || this.y > gameState.playerOldY + 2000 || this.y > gameState.playerOldY + 2000) {
+                    this.setActive(false);
+                    this.setVisible(false);
+                    this.destroy(); // find function to delete
+                    gameState.takeShootCharac = 0;
+                }
+                                
+            }
+        });
+
+        //Arrow clone create. (Player 2);
+        var Arrow2 = new Phaser.Class({
+
+            Extends: Phaser.Physics.Arcade.Image,
+
+            initialize:
+
+                function Bullet(scene) {
+                    Phaser.Physics.Arcade.Image.call(this, scene, 0, 0, 'axe');
+                    this.speed =  Phaser.Math.GetSpeed(400,1);
+                },
+
+            fire: function (x, y, type) {
+                this.type = type;
+                this.setPosition(x+30, y-4);
+                gameState.playerOldX = x;
+                gameState.playerOldY = y;
+                this.setActive(true);
+                this.setVisible(true);
+            },
+
+            update: function (time, delta) {
+                if (this.type === 'L') {
+                    let x = this.speed * delta;
+                    this.x -= x - 3;
+                    this.y += -4*x;
+                } 
+
+                if (this.x > gameState.playerOldX + 2000 || this.x < gameState.playerOldX - 2000 || gameState.takeShootCharac2 == 1 || this.y > gameState.playerOldY + 2000 || this.y > gameState.playerOldY + 2000) {
                     this.setActive(false);
                     this.setVisible(false);
                     this.destroy(); // find function to delete
@@ -71,12 +115,18 @@ class MatchGame extends Phaser.Scene {
             }
         });
 
+        //Create Arrow Clone Character 1.
         gameState.arrows = this.physics.add.group({
             classType: Arrow,
-            maxsize: 1,
+            maxsize: 5,
             runChildUpdate: true,
         });
-
+        //Create Arrow Clone Character 2.
+        gameState.arrows2 = this.physics.add.group({
+            classType: Arrow2,
+            maxsize: 5,
+            runChildUpdate: true,
+        });
         //gameState.arrow = gameState.arrows.get();
         //gameState.arrow.body.setAllowGravity(false);
 
@@ -85,12 +135,38 @@ class MatchGame extends Phaser.Scene {
         gameState.healthText2 = this.add.text(750, 0, '0 :Health',{font:'40px Times New Roman', fill: '#FFFFFF'});
 
         //Player touch Axe
+        this.physics.add.overlap(gameState.character, gameState.arrows2, () => {
+            gameState.healthCharacter += -100;
+            gameState.takeShootCharac2 = 1;
+        })
         this.physics.add.overlap(gameState.character2, gameState.arrows, () => {
             gameState.healthCharacter2 += -100;
-            gameState.takeShootCharac2 = 1;
+            gameState.takeShootCharac = 1;
+        })
+
+        //Create Animation
+        this.anims.create({
+            key:'walk',
+            frames: this.anims.generateFrameNumbers('character', {
+                start: 0,
+                end: 12,
+            }),
+            frameRate: 20,
+            repeat: -1,
+        });
+        this.anims.create({
+            key:'attack',
+            frames: this.anims.generateFrameNumbers('characterAttack', {
+                start: 0,
+                end: 17,
+            }),
+            frameRate: 17.5,
+            repeat: -1,
         })
     }
     update() {
+        
+
         //Health
         gameState.healthText.setText(`Health: ${gameState.healthCharacter}`);
         gameState.healthText2.setText(`${gameState.healthCharacter2} :Health`);
@@ -99,6 +175,8 @@ class MatchGame extends Phaser.Scene {
         gameState.cursors = this.input.keyboard.createCursorKeys();
         if (gameState.cursors.left.isDown) {
             gameState.character.setVelocityX(-gameState.speedCharacter);
+            gameState.character.anims.play('walk', true);
+            gameState.character.flipX = true;
             gameState.keyArrow = 0;
             clearTimeout(gameState.timeChargeFunction);
 
@@ -107,6 +185,8 @@ class MatchGame extends Phaser.Scene {
             gameState.isStop = false;
         } else if (gameState.cursors.right.isDown) {
             gameState.character.setVelocityX(gameState.speedCharacter);
+            gameState.character.anims.play('walk', true);
+            gameState.character.flipX = false;
             gameState.keyArrow = 0;
             clearTimeout(gameState.timeChargeFunction);
 
@@ -115,6 +195,8 @@ class MatchGame extends Phaser.Scene {
            gameState.isStop = false;
         } else {
             gameState.character.setVelocityX(0);
+            gameState.character.anims.play('attack', true);
+            gameState.character.flipX = false;
 
             // Send to client 
             if (!gameState.isStop)  {
@@ -124,7 +206,7 @@ class MatchGame extends Phaser.Scene {
 
         }
 
-        //Shoot Arrow
+        //Shoot Arrow (Player 1)
         if (gameState.cursors.right.isUp && gameState.cursors.left.isUp && gameState.keyArrow == 2) {
             gameState.keyArrow = 0;
             gameState.arrow = gameState.arrows.get();
@@ -142,7 +224,25 @@ class MatchGame extends Phaser.Scene {
             };
         };
 
-        //Charge Arrow (Take Arrow)
+        //Shoot Arrow (Player 2)
+    
+        if (gameState.keyArrow2 == 2) {
+            gameState.keyArrow2 = 0;
+            gameState.arrow2 = gameState.arrows2.get();
+            gameState.arrow2.setScale(0.05);
+            gameState.arrow2.setVelocityY(1200);
+            gameState.arrow2.setTint('0x55FF00');
+            gameState.arrow2.body.angularVelocity = 360 * 2;
+            // gameState.arrow.body.setAllowGravity(false);
+            if (gameState.arrow2 !== null) {
+                // gameState.arrow.body.setAllowGravity(false);
+
+                if (gameState.arrow2) {
+                    gameState.arrow2.fire(gameState.character2.x, gameState.character2.y, 'L');
+                }
+            };
+        };
+        //Charge Arrow (Take Arrow, Player 1)
         if (gameState.cursors.right.isUp && gameState.cursors.left.isUp && gameState.keyArrow == 0) {
             gameState.keyArrow = 1;
             gameState.timeChargeFunction = setTimeout(() => {
@@ -150,6 +250,16 @@ class MatchGame extends Phaser.Scene {
                     gameState.keyArrow = 2;  
                 }      
             },gameState.chargeTime);
+        }
+
+        //Charge Arrow (Take Arrow, Player 2)
+        if (gameState.keyArrow2 == 0) {
+            gameState.keyArrow2 = 1;
+            gameState.timeChargeFunction2 = setTimeout(() => {
+                if (gameState.keyArrow2 == 1) {
+                    gameState.keyArrow2 = 2;  
+                }      
+            },gameState.chargeTime2);
         }
 
         //Character die from falling
--- Archer/game.js
+++ Archer/game.js
@@ -3,12 +3,17 @@ const gameState = {
     widthScreen: 1000,
     heightScreen: 600,
     speedCharacter: 200,
+
     keyArrow: 0,
+    keyArrow2: 0,
+
     chargeTime: 1000,
+    chargeTime2: 1500,
 
     healthCharacter: 10000,
     healthCharacter2: 10000,
 
+    takeShootCharac: 0,
     takeShootCharac2: 0,
 
     isStop: true,
